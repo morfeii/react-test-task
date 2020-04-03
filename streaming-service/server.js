4a2ed21d4a23f8315d2e77d6b82a834f90ef7598
morfeii <morfeii88@gmail.com>
2020-04-03 11:31:37 +0300

    fix server.js

--- streaming-service/server.js
+++ streaming-service/server.js
@@ -1,81 +1,113 @@
-'use strict';
+'use strict'
 
 ////
 // CONFIGURATION SETTINGS
 ////
-var FETCH_INTERVAL = 5000;
-var PRETTY_PRINT_JSON = true;
+require('dotenv/config')
+
+const PORT = process.env.PORT || 4000
+const PRETTY_PRINT_JSON = true
+var UPDATE_INTERVAL = 5000
 
 ////
 // START
 ////
-var express = require('express');
-var http = require('http');
-var io = require('socket.io');
-var cors = require('cors');
+var express = require('express')
+var http = require('http')
+var io = require('socket.io')
+var cors = require('cors')
 
-function getRandomValBetween(min, max, precision) {
-  min = min === undefined ? 0 : min;
-  max = max === undefined ? 9007199254740992 : max;
-  precision = precision === undefined ? 0 : precision;
+var app = express()
+app.use(cors())
+var server = http.createServer(app)
 
-  var random = Math.random() * (max - min) + min;
-
-  return random.toFixed(precision);
-}
-
-function getUTCDate() {
-  var now = new Date();
-  return new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
-}
-
-function getQuote(socket, ticker) {
-  var dataObj;
-
-  var quote = {};
-  quote.ticker = ticker;
-  quote.exchange = 'NASDAQ';
-  quote.price = getRandomValBetween(100, 300, 2);
-  quote.change = getRandomValBetween(0, 200, 2);
-  quote.change_percent = getRandomValBetween(0, 1, 2);
-  quote.last_trade_time = getUTCDate();
-  quote.dividend = getRandomValBetween(0, 1, 2);
-  quote.yield = getRandomValBetween(0, 2, 2);
-
-  socket.emit(ticker, PRETTY_PRINT_JSON ? JSON.stringify(quote, null, 4) : JSON.stringify(quote));
-}
-
-function trackTicker(socket, ticker) {
-  console.log('track Ticker');
-
-  // run the first time immediately
-  getQuote(socket, ticker);
-
-  // every N seconds
-  var timer = setInterval(function() {
-    getQuote(socket, ticker);
-  }, FETCH_INTERVAL);
-
-  socket.on('disconnect', function() {
-    clearInterval(timer);
-  });
-}
-
-var app = express();
-app.use(cors());
-var server = http.createServer(app);
-
-var io = io.listen(server);
-io.set('origins', '*:*');
+var io = io.listen(server)
+io.set('origins', '*:*')
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
 io.sockets.on('connection', function(socket) {
   socket.on('ticker', function(ticker) {
-    trackTicker(socket, ticker);
-  });
-});
+    trackTicker(socket, ticker)
+  })
 
-server.listen(process.env.PORT || 4000);
+  socket.on('changeInterval', ms => {
+    if (ms < 0) throw new Error('The update interval cannot be less than zero')
+    UPDATE_INTERVAL = ms
+  })
+})
+
+server.listen(PORT, () => console.log('Server is running on port:', PORT))
+
+function getRandomValBetween(min, max, precision) {
+  min = min === undefined ? 0 : min
+  max = max === undefined ? 9007199254740992 : max
+  precision = precision === undefined ? 0 : precision
+
+  var random = Math.random() * (max - min) + min
+
+  return random.toFixed(precision)
+}
+
+function getUTCDate() {
+  var now = new Date()
+  return new Date(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds()
+  )
+}
+
+function generateQuote(stockSymbol) {
+  return {
+    stockSymbol,
+    exchange: 'NASDAQ',
+    price: getRandomValBetween(250, 300, 2),
+    change: getRandomValBetween(0, 200, 2),
+    change_percent: getRandomValBetween(0, 1, 2),
+    last_trade_time: getUTCDate(),
+    dividend: getRandomValBetween(0, 1, 2),
+    yield: getRandomValBetween(0, 2, 2),
+    updateInterval: UPDATE_INTERVAL
+  }
+}
+
+function sendQuote(socket, stockSymbol) {
+  const quote = generateQuote(stockSymbol)
+
+  socket.emit(
+    stockSymbol,
+    PRETTY_PRINT_JSON ? JSON.stringify(quote, null, 4) : JSON.stringify(quote)
+  )
+}
+
+function trackTicker(socket, stockSymbol) {
+  let interval = null
+
+  const runUpdating = () => {
+    const time = UPDATE_INTERVAL
+    interval = setInterval(() => {
+      if (time == UPDATE_INTERVAL) {
+        sendQuote(socket, stockSymbol)
+        return
+      }
+
+      // Clear and set new interval time
+      clearInterval(interval)
+      runUpdating()
+    }, UPDATE_INTERVAL)
+  }
+
+  // run the first time immediately
+  sendQuote(socket, stockSymbol)
+  runUpdating()
+
+  socket.on('disconnect', function() {
+    clearInterval(interval)
+  })
+}
